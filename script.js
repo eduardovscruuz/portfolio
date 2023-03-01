da263b6d4164d410b454174eeb24760694f1b449
eduardovscruuz <eduardovscruuz@gmail.com>
2023-02-28 22:39:58 -0300

    desgin e rd att

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-function hover(img)
-{
-    img.src = "/media/github-black.png"
-}
+const git = document.getElementById("github-img");
+const hack = document.getElementById("hackerrank-img");
 
-function hoverOut(img){
-    img.src = "/media/github.png"
-}
+const overGit = () => {
+	git.src = "/media/github-black.png";
+};
+
+const outGit = () => {
+	git.src = "/media/github.png";
+};
+
+const overHack = () => {
+	hack.src = "/media/hackerrank2.png.PNG";
+};
+
+const outHack = () => {
+	hack.src = "/media/hackerrank1.png.PNG";
+};
+
+git.addEventListener("mouseover", overGit);
+git.addEventListener("mouseout", outGit);
+
+hack.addEventListener("mouseover", overHack);
+hack.addEventListener("mouseout", outHack);
