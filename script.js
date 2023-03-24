75de302d415afc516fd9603161a2b6bc4bdf1678
eduardovscruuz <eduardovscruuz@gmail.com>
2023-03-24 00:17:11 -0300

    structured

--- script.js
+++ script.js
@@ -3,43 +3,72 @@ const box2 = document.querySelector(".mini-container2");
 const box3 = document.querySelector(".mini-container3");
 const box4 = document.querySelector(".mini-container4");
 const box5 = document.querySelector(".mini-container5");
-const text = document.querySelector("span");
-const about = document.querySelector(".about");
+const contentContainer1 = document.querySelector(".content-container1");
+const contentContainer2 = document.querySelector(".content-container2");
+const contentContainer3 = document.querySelector(".content-container3");
+const contentContainer4 = document.querySelector(".content-container4");
+const contentContainer5 = document.querySelector(".content-container5");
+const content = document.querySelector(".content");
 
 const changePadding1 = () => {
 	box1.style.paddingRight = "";
+	contentContainer1.style.display = "";
 	box2.style.padding = "0vw";
 	box3.style.padding = "";
 	box4.style.padding = "";
+	contentContainer2.style.display = "none";
+	contentContainer3.style.display = "none";
+	contentContainer4.style.display = "none";
+	contentContainer5.style.display = "none";
 };
-
 const changePadding2 = () => {
 	box2.style.paddingRight = "75vw";
+	contentContainer2.style.display = "flex";
 	box1.style.padding = "0vw";
 	box3.style.padding = "0vw";
 	box4.style.padding = "0vw";
 	box5.style.padding = "0vw";
+	contentContainer1.style.display = "none";
+	contentContainer3.style.display = "none";
+	contentContainer4.style.display = "none";
+	contentContainer5.style.display = "none";
 };
 
 const changePadding3 = () => {
 	box3.style.paddingRight = "75vw";
+	contentContainer3.style.display = "flex";
 	box1.style.padding = "0vw";
 	box2.style.padding = "0vw";
 	box4.style.padding = "0vw";
 	box5.style.padding = "0vw";
+	contentContainer2.style.display = "none";
+	contentContainer1.style.display = "none";
+	contentContainer4.style.display = "none";
+	contentContainer5.style.display = "none";
 };
 
 const changePadding4 = () => {
 	box4.style.paddingRight = "75vw";
+	contentContainer4.style.display = "flex";
 	box1.style.padding = "0vw";
 	box2.style.padding = "0vw";
 	box3.style.padding = "0vw";
 	box5.style.padding = "0vw";
+	contentContainer1.style.display = "none";
+	contentContainer2.style.display = "none";
+	contentContainer3.style.display = "none";
+	contentContainer5.style.display = "none";
 };
+
 const changePadding5 = () => {
 	box5.style.paddingRight = "75vw";
+	contentContainer5.style.display = "flex";
 	box1.style.padding = "0vw";
 	box2.style.padding = "0vw";
 	box3.style.padding = "0vw";
 	box4.style.padding = "0vw";
+	contentContainer1.style.display = "none";
+	contentContainer2.style.display = "none";
+	contentContainer3.style.display = "none";
+	contentContainer4.style.display = "none";
 };
