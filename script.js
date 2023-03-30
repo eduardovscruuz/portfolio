dc3fafdce20baee96dd4e3573d458f7ee22c4251
eduardovscruuz <eduardovscruuz@gmail.com>
2023-03-29 21:23:52 -0300

    cleaner code

--- script.js
+++ script.js
@@ -1,75 +1,93 @@
-const box1 = document.querySelector(".mini-container1");
-const box2 = document.querySelector(".mini-container2");
-const box3 = document.querySelector(".mini-container3");
-const box4 = document.querySelector(".mini-container4");
-const box5 = document.querySelector(".mini-container5");
-const contentContainer1 = document.querySelector(".content-container1");
-const contentContainer2 = document.querySelector(".content-container2");
-const contentContainer3 = document.querySelector(".content-container3");
-const contentContainer4 = document.querySelector(".content-container4");
-const contentContainer5 = document.querySelector(".content-container5");
-const content = document.querySelector(".content");
+const homeContainer = document.querySelector(".home-container");
+const aboutContainer = document.querySelector(".about-container");
+const skillsContainer = document.querySelector(".skills-container");
+const projectsContainer = document.querySelector(".projects-container");
+const contactContainer = document.querySelector(".contact-container");
 
-const changePadding1 = () => {
-	box1.style.paddingRight = "";
-	contentContainer1.style.display = "";
-	box2.style.padding = "0vw";
-	box3.style.padding = "";
-	box4.style.padding = "";
-	box5.style.padding = "0vw";
-	contentContainer2.style.display = "none";
-	contentContainer3.style.display = "none";
-	contentContainer4.style.display = "none";
-	contentContainer5.style.display = "none";
-};
-const changePadding2 = () => {
-	box2.style.paddingRight = "75vw";
-	contentContainer2.style.display = "flex";
-	box1.style.padding = "0vw";
-	box3.style.padding = "0vw";
-	box4.style.padding = "0vw";
-	box5.style.padding = "0vw";
-	contentContainer1.style.display = "none";
-	contentContainer3.style.display = "none";
-	contentContainer4.style.display = "none";
-	contentContainer5.style.display = "none";
+const homeColumn = document.querySelector(".home-column");
+const aboutColumn = document.querySelector(".about-column");
+const skillsColumn = document.querySelector(".skills-column");
+const projectsColumn = document.querySelector(".projects-column");
+const contactColumn = document.querySelector(".contact-column");
+
+const homeContent = document.querySelector(".home-container-content");
+const aboutContent = document.querySelector(".about-container-content");
+const skillsContent = document.querySelector(".skills-container-content");
+const projectsContent = document.querySelector(".projects-container-content");
+const contactContent = document.querySelector(".contact-container-content");
+
+const slideHome = () => {
+	homeContainer.style.paddingRight = "";
+	aboutContainer.style.padding = "0vw";
+	skillsContainer.style.padding = "";
+	projectsContainer.style.padding = "";
+	contactContainer.style.padding = "0vw";
+
+	homeContent.style.display = "";
+	aboutContent.style.display = "none";
+	skillsContent.style.display = "none";
+	projectsContent.style.display = "none";
+	contactContent.style.display = "none";
 };
 
-const changePadding3 = () => {
-	box3.style.paddingRight = "75vw";
-	contentContainer3.style.display = "flex";
-	box1.style.padding = "0vw";
-	box2.style.padding = "0vw";
-	box4.style.padding = "0vw";
-	box5.style.padding = "0vw";
-	contentContainer2.style.display = "none";
-	contentContainer1.style.display = "none";
-	contentContainer4.style.display = "none";
-	contentContainer5.style.display = "none";
+const slideAbout = () => {
+	homeContainer.style.padding = "0vw";
+	aboutContainer.style.paddingRight = "75vw";
+	skillsContainer.style.padding = "0vw";
+	projectsContainer.style.padding = "0vw";
+	contactContainer.style.padding = "0vw";
+
+	homeContent.style.display = "none";
+	aboutContent.style.display = "flex";
+	skillsContent.style.display = "none";
+	projectsContent.style.display = "none";
+	contactContent.style.display = "none";
 };
 
-const changePadding4 = () => {
-	box4.style.paddingRight = "75vw";
-	contentContainer4.style.display = "flex";
-	box1.style.padding = "0vw";
-	box2.style.padding = "0vw";
-	box3.style.padding = "0vw";
-	box5.style.padding = "0vw";
-	contentContainer1.style.display = "none";
-	contentContainer2.style.display = "none";
-	contentContainer3.style.display = "none";
-	contentContainer5.style.display = "none";
+const slideSkills = () => {
+	homeContainer.style.padding = "0vw";
+	aboutContainer.style.padding = "0vw";
+	skillsContainer.style.paddingRight = "75vw";
+	projectsContainer.style.padding = "0vw";
+	contactContainer.style.padding = "0vw";
+
+	homeContent.style.display = "none";
+	aboutContent.style.display = "none";
+	skillsContent.style.display = "flex";
+	projectsContent.style.display = "none";
+	contactContent.style.display = "none";
 };
 
-const changePadding5 = () => {
-	box5.style.paddingRight = "75vw";
-	contentContainer5.style.display = "flex";
-	box1.style.padding = "0vw";
-	box2.style.padding = "0vw";
-	box3.style.padding = "0vw";
-	box4.style.padding = "0vw";
-	contentContainer1.style.display = "none";
-	contentContainer2.style.display = "none";
-	contentContainer3.style.display = "none";
-	contentContainer4.style.display = "none";
+const slideProjects = () => {
+	homeContainer.style.padding = "0vw";
+	aboutContainer.style.padding = "0vw";
+	skillsContainer.style.padding = "0vw";
+	projectsContainer.style.paddingRight = "75vw";
+	contactContainer.style.padding = "0vw";
+
+	homeContent.style.display = "none";
+	aboutContent.style.display = "none";
+	skillsContent.style.display = "none";
+	projectsContent.style.display = "flex";
+	contactContent.style.display = "none";
 };
+
+const slideContact = () => {
+	homeContainer.style.padding = "0vw";
+	aboutContainer.style.padding = "0vw";
+	skillsContainer.style.padding = "0vw";
+	projectsContainer.style.padding = "0vw";
+	contactContainer.style.paddingRight = "75vw";
+
+	homeContent.style.display = "none";
+	aboutContent.style.display = "none";
+	skillsContent.style.display = "none";
+	projectsContent.style.display = "none";
+	contactContent.style.display = "flex";
+};
+
+homeColumn.addEventListener("click", slideHome);
+aboutColumn.addEventListener("click", slideAbout);
+skillsColumn.addEventListener("click", slideSkills);
+projectsColumn.addEventListener("click", slideProjects);
+contactColumn.addEventListener("click", slideContact);
