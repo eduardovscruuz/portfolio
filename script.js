5f4d9ab9341e9b7f9da18da32770c2604840bd79
eduardovscruuz <eduardovscruuz@gmail.com>
2025-01-09 20:24:29 -0300

    visual web finalizado

--- script.js
+++ script.js
@@ -234,8 +234,8 @@ if (window.matchMedia('(max-width: 800px)').matches) {
     <div class="info">
       <h3>${item.name}</h3>
       <ul>${techsList}</ul>
-      <a href="${item.linkGit}" target="_blank">See the code</a>
-      <a href="${item.linkSite}" target="_blank">See it running</a>
+      <a data-translate="projBtn1" href="${item.linkGit}" target="_blank"></a>
+      <a data-translate="projBtn2" href="${item.linkSite}" target="_blank"></a>
     </div>
   `;
 
@@ -325,3 +325,96 @@ for (let i = 0; i < toggleButtons.length; i++) {
     themeStylesheet.setAttribute('href', currentTheme === 'css/styles.css' ? 'css/lightStyles.css' : 'css/styles.css');
   });
 }
+
+const translations = {
+  en: {
+    homeTitle: 'Home',
+    title: "HELLO, I'M <span>EDUARDO</span>",
+    description: 'FULL STACK DEVELOPER, WELCOME TO MY PORTFOLIO!',
+    resume: 'Resume',
+    skillsTitle: 'Skills',
+    techInfo:
+      'My tech stack emphasizes strongly typed languages and <span>object-oriented programming</span>, ensuring both robustness and <span>scalability</span>. On the front end, I leverage TypeScript with Angular and SASS to build elegant, functional interfaces. On the back end, I specialize in NestJS, C#, and .NET to deliver efficient, well-structured APIs. With experience in relational and non-relational databases, <span>I tailor the best solutions</span> to fit each project’s needs.',
+    aboutTitle: 'About Me',
+    meText1:
+      'My name is Eduardo Cruz, a student of Systems Analysis and Development, and a full-stack developer at the beginning of my career. I’ve been in the programming field since 2023, and during this time, I’ve had the opportunity to explore a bit of each of the main technologies, which allowed me to identify the ones where I can deliver the best results.',
+    meText2:
+      'Outside of my professional life, I can say that I’m an enthusiast of Stoic philosophy, cinema, cooking, and, of course, technology in its various forms. I believe these interests complement my creative and logical vision, which reflects in the way I approach challenges both in programming and in my personal life.',
+
+    projectsTitle: 'Projects',
+    projBtn1: 'See the code',
+    projBtn2: 'See it running',
+    contactTitle: 'Contact',
+    chat1: "Let's chat!",
+    chat2: 'Wheter you have a question, want to start new project, or simply wants to connect.',
+    chat3:
+      'Feel free to send me a message in the contact form, or directly send me an e-mail in: <a href="mailto:eduardovscruuz@gmail.com" >eduardovscruuz@gmail.com</a>',
+    name_placeholder: 'MY NAME IS',
+    email_placeholder: 'MY E-MAIL IS',
+    subject_placeholder: 'SUBJECT',
+    message_placeholder: "I'D LIKE TO CHAT ABOUT",
+    submit_value: 'Send Message',
+  },
+  pt: {
+    homeTitle: 'Início',
+    title: 'OLÁ, EU SOU <span>EDUARDO</span>',
+    description: 'Desenvolvedor Full Stack, bem-vindo(a) ao meu portfólio!',
+    resume: 'Currículo',
+    skillsTitle: 'Habilidades',
+    techInfo:
+      'Minha stack de tecnologias enfatiza linguagens fortemente tipadas e <span>programação orientada a objetos</span>, garantindo tanto a robustez quanto a <span>escalabilidade</span>. No front-end, utilizo TypeScript com Angular e SASS para criar interfaces elegantes e funcionais. No back-end, sou especializado em NestJS, C# e .NET, entregando APIs eficientes e bem estruturadas. Com experiência em bancos de dados relacionais e não relacionais, <span>adapto as melhores soluções</span> às necessidades de cada projeto.',
+    aboutTitle: 'Sobre Mim',
+    meText1:
+      'Me chamo Eduardo Cruz, estudante de Análise e Desenvolvimento de Sistemas e desenvolvedor full stack em início de carreira. Estou na área da programação desde 2023 e, desde então, tive a oportunidade de explorar um pouco de cada uma das principais tecnologias, o que me permitiu identificar aquelas nas quais consigo entregar os melhores resultados.',
+    meText2:
+      'Fora do âmbito profissional, posso dizer que sou um entusiasta da filosofia estóica, do cinema, da culinária e, claro, tecnologia em suas diversas formas. Acredito que esses interesses complementam minha visão criativa e lógica, o que reflete na forma como abordo os desafios tanto na programação quanto na vida pessoal.',
+
+    projectsTitle: 'Projetos',
+    projBtn1: 'Ver o código',
+    projBtn2: 'Ver o site',
+    contactTitle: 'Contato',
+    chat1: 'Vamos conversar!',
+    chat2: 'Se você tiver uma pergunta, quiser começar um novo projeto ou simplesmente se conectar.',
+    chat3:
+      'Sinta-se à vontade para me enviar uma mensagem no formulário de contato ou enviar um e-mail diretamente para: <a href="mailto:eduardovscruuz@gmail.com">eduardovscruuz@gmail.com</a>',
+    name_placeholder: 'MEU NOME É',
+    email_placeholder: 'MEU E-MAIL É',
+    subject_placeholder: 'ASSUNTO',
+    message_placeholder: 'GOSTARIA DE CONVERSAR SOBRE',
+    submit_value: 'Enviar Mensagem',
+  },
+};
+// Função para alterar o idioma
+function changeLanguage(lang) {
+  document.getElementById('name_input').setAttribute('placeholder', translations[lang].name_placeholder);
+  document.getElementById('email_input').setAttribute('placeholder', translations[lang].email_placeholder);
+  document.getElementById('subject_input').setAttribute('placeholder', translations[lang].subject_placeholder);
+  document.getElementById('message_input').setAttribute('placeholder', translations[lang].message_placeholder);
+
+  // Atualizar o valor do botão de envio
+  document.getElementById('form_button').setAttribute('value', translations[lang].submit_value);
+  // Selecionar todos os elementos que precisam de tradução
+  const translatableElements = document.querySelectorAll('[data-translate]');
+
+  translatableElements.forEach((element) => {
+    const key = element.getAttribute('data-translate');
+    if (translations[lang][key]) {
+      element.innerHTML = translations[lang][key];
+    }
+  });
+
+  // Gerenciar classes nos botões de idioma
+  const btnEn = document.querySelectorAll('.en'); // Seleciona todos os elementos com classe 'en'
+  const btnPt = document.querySelectorAll('.pt'); // Seleciona todos os elementos com classe 'pt'
+
+  if (lang === 'en') {
+    btnEn.forEach((btn) => btn.classList.remove('disabled-lang')); // Adiciona a classe desabilitada a todos os botões EN
+    btnPt.forEach((btn) => btn.classList.add('disabled-lang')); // Remove a classe desabilitada de todos os botões PT
+  } else if (lang === 'pt') {
+    btnPt.forEach((btn) => btn.classList.remove('disabled-lang')); // Adiciona a classe desabilitada a todos os botões PT
+    btnEn.forEach((btn) => btn.classList.add('disabled-lang')); // Remove a classe desabilitada de todos os botões EN
+  }
+}
+
+// Define um idioma padrão ao carregar a página
+window.onload = () => changeLanguage('pt');
